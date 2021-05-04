b6dd9e972cb7b7a4f9f39ee2b630a31d2a7ac1db
Erick <19063989@student.hhs.nl>
2021-05-04 17:25:45 +0200

    Test aangemaakt

---
+++ App/Bestelling.java
@@ -0,0 +1,12 @@
+public class Bestelling {
+    private int totaal;
+    private int prijs;
+
+    public int besteld(){
+        return prijs = 1;
+    }
+
+    public int totaalBesteld(){
+         return totaal = besteld() + 5;
+    }
+}
---
+++ Test/BestellingTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BestellingTest
+{
+    @Test
+    public void BestellingGeplaatstTest(){
+        Bestelling bestelling = new Bestelling();
+        Assert.assertEquals(1,bestelling.besteld());
+    }
+
+    @Test
+    public void totaalBesteldTest(){
+        Bestelling bestelling = new Bestelling();
+        Assert.assertEquals(6,bestelling.totaalBesteld());
+    }
+}

b54c5fe66f31177b9783898243b0a0b16b09f14a
Erick <19063989@student.hhs.nl>
2021-05-08 22:57:20 +0200

    Login in the Main

--- App/Gebruiker.java
+++ App/Gebruiker.java
@@ -2,4 +2,8 @@ public class Gebruiker extends Persoon{
     public Gebruiker(int id, String naam, String achternaam, String telefoonnummer, String email, String gebruikersnaam, String wachtwoord) {
         super(id, naam, achternaam, telefoonnummer, email, gebruikersnaam, wachtwoord);
     }
+
+    public static boolean loginControleGebruiker(){
+        if ()
+    }
 }
--- App/Main.java
+++ App/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+import static java.lang.System.in;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(in);
+        System.out.println("Login als Gebruiker [1], Medewerker [2]");
+        String input = s.nextLine();
+        if(input.equals("1")){
+            Gebruiker.loginControleGebruiker();
+        } else if(input.equals("2")){
+            Gebruiker.loginControleGebruiker();
+        } else {
+            exit(0);
+        }
     }
 }

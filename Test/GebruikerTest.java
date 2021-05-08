6613936ca0b7019e620ac27ae2b2ad88ed3bbb59
Erick <19063989@student.hhs.nl>
2021-05-08 23:29:14 +0200

    Test voor gebruiker

--- Test/GebruikerTest.java
+++ Test/GebruikerTest.java
@@ -5,11 +5,11 @@ import static org.junit.Assert.*;
 
 public class GebruikerTest {
 
-    @Test // Test om te kijken of een gebruiker bestaat
-    public void loginControleGebruiker() {
+    @Test // Test om te kijken of een gebruiker bestaat op gebruikersnaam
+    public void loginControleGebruikerTest() {
         Gebruiker gebruiker = new Gebruiker(3,"tom","kramer",
                 "0612345678","tom@email.nl","tom123","123");
-        String expected = ;
-        Assert.assertEquals(1,1);
+        String expected = "tom123";
+        Assert.assertEquals(expected,gebruiker.getGebruikersnaam());
     }
 }

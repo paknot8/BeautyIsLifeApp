1df3c2ba4e407c546eea6ea32d65e737b8f310e0
Erick <19063989@student.hhs.nl>
2021-05-08 22:25:49 +0200

    Class Gebruiker

---
+++ App/Gebruiker.java
@@ -0,0 +1,7 @@
+public class Gebruiker extends Persoon{
+    public Gebruiker(String naam, String achternaam, String email, String gebruikersnaam, String wachtwoord) {
+        super(naam, achternaam, email, gebruikersnaam, wachtwoord);
+    }
+
+
+}

6568205450bcdf1dfc438bb0f40c80e55b6e1d88
Erick <19063989@student.hhs.nl>
2021-06-10 23:21:37 +0200

    int i

--- Bestellingen/Bestelling.java
+++ Bestellingen/Bestelling.java
@@ -2,6 +2,7 @@ import java.util.InputMismatchException;
 
 public class Bestelling
 {
+    private static int i;
     public static int newVoorraad;
     public static int userInputAantalBesteld;
     // Constructor Data
@@ -84,11 +85,9 @@ public class Bestelling
 
     // Door de lijst van producten gaan
     private static void loopProductenLijst(){
-        for (int i = 0; i < ProductData.ProductenLijst.size(); i++) {
+        for (i = 0; i < ProductData.ProductenLijst.size(); i++) {
             int tempProductID = ProductData.ProductenLijst.get(i).getProductId();
-            TempField.tempProductNaam = ProductData.ProductenLijst.get(i).getProductNaam();
-            TempField.tempProductPrijs = ProductData.ProductenLijst.get(i).getProductPrijs();
-            TempField.tempProductVoorraad = ProductData.ProductenLijst.get(i).getProductVoorraad();
+            productTemporary();
             if(TempField.tempProductNaam.equals(TempField.userInput)){
                 VoorraadBerekenen.newVoorraadBerekenen(); // bereken de nieuwe voorraad (oud voorraad - aantal besteld = new vooraad)
                 Product product = new Product(tempProductID, TempField.tempProductNaam,TempField.tempProductPrijs,newVoorraad);
@@ -96,6 +95,12 @@ public class Bestelling
                 addToMijnBestelLijst(); // Voegt in mijn bestellijst
             }
         }
+    }
+
+    private static void productTemporary(){
+        TempField.tempProductNaam = ProductData.ProductenLijst.get(i).getProductNaam();
+        TempField.tempProductPrijs = ProductData.ProductenLijst.get(i).getProductPrijs();
+        TempField.tempProductVoorraad = ProductData.ProductenLijst.get(i).getProductVoorraad();
     }
 
     // Voegt in bestellijst toe.
